ee109c56d8778fbd7a621447bb5a74021a88276f
Pradana Aumars <pradana.aumars@courrier.dev>
2024-12-10 22:01:09 +0100

    refactor: clean day 3 code

--- adc2024/day03.ml
+++ adc2024/day03.ml
@@ -7,18 +7,16 @@ let compute_mul_instructions str cond =
     try
       let new_pos = Str.search_forward r str pos in
       let match_str = Str.matched_string str in
-      if String.compare match_str "do()" = 0 then
-        find_matches (new_pos + String.length match_str) (acc1, acc2) true
-      else if String.compare match_str "don't()" = 0 then
-        find_matches (new_pos + String.length match_str) (acc1, acc2) false
-      else
-        let factor1, factor2 = scan match_str in
-        if cond then
-          find_matches (new_pos + String.length match_str) (acc1 + factor1 * factor2, acc2 + factor1 * factor2) cond
-        else
-          find_matches (new_pos + String.length match_str) (acc1 + factor1 * factor2, acc2) cond
+      match match_str with
+      | "do()" -> find_matches (new_pos + String.length match_str) (acc1, acc2) true
+      | "don't()" -> find_matches (new_pos + String.length match_str) (acc1, acc2) false
+      | _ ->
+          let factor1, factor2 = scan match_str in
+          let new_acc1 = acc1 + factor1 * factor2 in
+          let new_acc2 = if cond then acc2 + factor1 * factor2 else acc2 in
+          find_matches (new_pos + String.length match_str) (new_acc1, new_acc2) cond
     with Not_found ->
-      acc1, acc2, cond
+      (acc1, acc2, cond)
   in
   find_matches 0 (0, 0) cond
 
@@ -27,11 +25,11 @@ let parse_input input =
   try
     let f (acc1, acc2, cond) s =
       let res1, res2, cond = compute_mul_instructions s cond in
-      acc1 + res1, acc2 + res2, cond
+      (acc1 + res1, acc2 + res2, cond)
     in
     let res1, res2, _ = In_channel.fold_lines f (0, 0, true) ic in
     close_in ic;
-    res1, res2
+    (res1, res2)
   with e ->
     close_in_noerr ic;
     raise e
